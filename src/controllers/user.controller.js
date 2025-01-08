2f95561e632d59a816e973c05d7e61b8b5ae2df1
Yojjal Sharma <yojjal878@gmail.com>
2025-01-08 19:45:56 +0545

    remove unused import from user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,6 @@ import { User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 import  jwt  from "jsonwebtoken";
-import { mongo } from "mongoose";
 
 
 const generateAccessAndRefereshTokens = async (userId) => {
@@ -466,3 +465,4 @@ export {
     getUserChannelProfile,
     getWatchHistory
 }
+
